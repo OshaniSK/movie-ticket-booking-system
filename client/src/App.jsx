74c06aea7cecc791310a197eef7c4acd1eb57c94
Oshani <oshanirangi99@gmail.com>
2026-07-24 16:23:36 +0530

    feat: implement main application structure with routing and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import SeatLayout from './pages/SeatLayout';
+import MyBookings from './pages/MyBookings';
+import Favorite from './pages/Favorite';
+import Movies from './pages/Movies';
+import MovieDetails from './pages/MovieDetails';
+import { Routes, Route } from 'react-router-dom';
+import {Toaster} from 'react-hot-toast';
+import Footer from './components/Footer';
+
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+     <Toaster/>
+     <Navbar/>
+     <Routes>
+       <Route path='/' element={<Home/>} />
+      <Route path='/movies' element={<Movies/>} />
+      <Route path='/movies/:id' element={<MovieDetails/>} />
+      <Route path='/movies/:id/:date' element={<SeatLayout/>} />
+      <Route path='/my-bookings' element={<MyBookings/>} />
+      <Route path='/favorite' element={<Favorite/>} />
+
+    </Routes>
+    <Footer/>
+
+    </>
   );
 }
 
---
+++ client/src/pages/Movies.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Movies = () => {
+  return (
+    <div className="pt-24 px-6 md:px-16 lg:px-36 text-white">
+      <h1 className="text-2xl font-semibold">Movies</h1>
+    </div>
+  );
+};
+
+export default Movies;
